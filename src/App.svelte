8ec5818ed6e1d5b099fcf68b855d973921ba1443
shiggan <1378432+shiggan@users.noreply.github.com>
2022-11-05 16:36:13 +1300

    Adding About Page; wiring into routng

--- src/App.svelte
+++ src/App.svelte
@@ -13,10 +13,12 @@
   } from "carbon-components-svelte";
 
   import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
 
   let isSideNavOpen = false;
 </script>
 
+<!--  -->
 <div use:links>
   <Router url="">
     <Header
@@ -31,6 +33,7 @@
 
       <HeaderNav>
         <HeaderNavItem text="Home" href="/" />
+        <HeaderNavItem text="About" href="/about" />
       </HeaderNav>
 
       <SideNav bind:isOpen={isSideNavOpen}>
@@ -40,12 +43,18 @@
             href="/"
             on:click={() => (isSideNavOpen = !isSideNavOpen)}
           />
+          <SideNavLink
+            text="About"
+            href="/about"
+            on:click={() => (isSideNavOpen = !isSideNavOpen)}
+          />
         </SideNavItems>
       </SideNav>
     </Header>
 
     <Content>
       <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
       <Route let:location>
         <InlineNotification
           hideCloseButton
